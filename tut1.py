1f2689efab1ede43acc5777de0beeace0b1edb5e
raineedust <rayneedust@gmail.com>
2022-01-31 18:06:41 +0800

    Upload previous tuts

---
+++ tut1.py
@@ -0,0 +1,114 @@
+# 1
+
+# 4 * 2 - 4 - 2
+
+# 2
+
+# 2
+
+# 5** 3 – 5 // 2
+
+# SyntaxError: invalid character '–' (U+2013)
+
+# 3
+
+# -32 / 2**3 // 4 // 2
+
+# -1.0
+
+# 4
+
+# 26 % 2 + 13 /  2 ** 3
+
+# 1.625
+
+# 5
+
+# print(24 // 6 % 3 , 24 // 4 // 2)
+
+# 1 3
+
+# 6
+
+# x='2.3'
+# y='1.5'
+
+# z='8.0'
+
+# print(x,y,z)
+
+# 2.3 1.5 8.0
+
+# 7
+
+# print("1. I like python")
+# print('2. I like python')
+# print('3. I like python")
+# print("4. I like python")
+# print(5. I like python)
+# print("6. I, like, python")
+# print(("7. I, like, python"))
+
+# 1. I like python
+# 2. I like python
+# 3. SyntaxError: unterminated string literal (detected at line 1)
+# 4. I like python
+# 5. SyntaxError: invalid syntax. Perhaps you forgot a comma?
+# 6. I, like, python
+# 7. I, like, python
+
+# 8
+
+# print('I',end=' ') #one space
+# print('like',end=' ')#one space
+# print('computer',end='*')
+# print('programming')
+
+# I like computer*programming
+
+# 9
+
+# print(20%3,   end='    ')
+# print(-3//2.0)
+
+# 2 -2.0
+
+# 10
+
+# print('Good','morning','friends',sep=' * ')
+
+# Good * morning * friends
+
+# 11
+
+# print('We become what we think about',end='')
+# print('\n–Earl Nightingale')
+
+# We become what we think about –Earl Nightingale
+
+# 12
+
+# print(
+#     "Life isn't about getting and having, it's about giving and being.\n – Kevin Kruse"
+# )
+
+# 13
+
+# print("1\t2\t3\t4")
+# print("*\t*\t*\t*")
+
+# 14
+
+# print("************************")
+# print("* *")
+# print("* Grade : A *")
+# print("* *")
+# print("* *")
+# print("************************")
+
+# 15
+
+# print("mon\ttue\twed\tthu\tfri")
+# print("1\t2\t3\t4\t5")
+# print("6\t7\t8\t9\t10")
+# print("11\t12\t13\t14\t15")
