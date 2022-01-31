38dea7120ad487b4004ae9a39502f357e58b7cd4
raineedust <rayneedust@gmail.com>
2022-01-31 17:43:12 +0800

    Upload previous tuts

---
+++ tut2.py
@@ -0,0 +1,128 @@
+# Tutorial 2
+
+# 1
+# print(format(12356.29, ".1f"))
+# print(format(12356.899, ".2f"))
+# print(format(12356.899, ".3f"))
+# print(format(12356.8990, ".3f"))
+# print(format(12356.8999, ".3f"))
+# print(round(12356.879))
+# print(round(12359.809))
+
+# 12356.3
+# 12356.90
+# 12356.899
+# 12356.899
+# 12356.900
+# 12357
+# 12360
+
+# 2
+# int, float, str
+
+# 3
+# print("Hello" + "how are" + "you?")
+
+# Hellohow areyou?
+
+# 4
+# myValue = 99
+# myValue = 10
+# myValue = myValue + 15
+# print(myValue)
+
+# 25
+
+# 5
+
+# Number1 = 100
+# Number2 = 25
+# Product = Number1 * number2
+
+# NameError
+
+# 6
+
+# value1 = 2.6
+# value2 = int(value1)
+# value3 = -2.9
+# value4 = int(value3)
+# value5 = 2
+# value6 = float(value5)
+# print("\n", value2, "\n", value4, "\n", value6)
+
+# 2
+# -2
+# 2.0
+
+# 7
+
+# amountDue = 5000.0
+# monthlyPayment = amountDue / 12
+# print("The monthly payment is", format(monthlyPayment, ".2f"))
+
+# The monthly payment is 416.67
+
+# 8
+
+# score = 99
+# name = input("Enter your name :")
+# print(name, "your score is", score)
+
+# 1 3 2
+
+# 9
+
+# PI = 3.14159  # constant variable
+# r = float(input("Enter radius"))
+# area = PI * r ** 2
+# print("Area is", area)
+
+# 10
+
+# ProductCode = "ABC1190"
+# ProductName = "Sony DVD Player"
+# ProductPrice = str(350.00)
+# print(ProductCode + " " + ProductName + " RM " + ProductPrice)
+
+# 11
+
+# firstName = "John"
+# lastName = "Kevin"
+# print(firstName, lastName)
+
+# 12
+
+# A = 5
+# B = 10
+# C = B % A
+# print(C % 0)
+
+# In Mathematics, when a number is divided by a zero, the result is an infinite number.
+
+# 13
+
+# firstName = input("Enter your first name:")
+# lastName = input("Enter your last name:")
+# print("Hello", firstName, ",", lastName)
+
+# 14
+
+# name = input("Enter your name:")
+# yearOfBirth = int(input("Enter your year of birth YYYY:"))
+# yearOld = 2022 - yearOfBirth
+# print("Hi", name, "you're", yearOld, "years old")
+
+# 15
+
+# import math
+
+# # Gather data
+
+# radius = float(input("Enter the cone radius:"))
+# height = float(input("Enter the cone height:"))
+
+# # Input formula and display result
+
+# volume = math.pi * radius ** 2 * height / 3
+# print("The cone volume is:", volume)
---
+++ tut3.py
@@ -0,0 +1,58 @@
+# 1
+
+# value = int(input("Enter your number:"))
+# if value >= 0:
+#     print("It's a positive number")
+
+# else:
+#     print("It's a negative number")
+
+# 2
+
+# value = int(input("Enter your number:"))
+
+# if value % 2 == 0:
+#     print("even number")
+
+# else:
+#     print("odd number")
+
+# 3
+
+# print("Enter the length and width of the first rectangle below")
+# length1 = int(input("1st rectangle length:"))
+# width1 = int(input("1st rectangle width:"))
+
+# print("Enter the length and width of the second rectangle below")
+# length2 = int(input("2nd rectangle length:"))
+# width2 = int(input("2nd rectangle width:"))
+
+# area1 = length1 * width1
+# area2 = length2 * width2
+
+# if area1 == area2:
+#     print("The areas are the same.")
+# else:
+#     print("The areas are not the same.")
+
+# 4
+
+# annualSalary = int(input("Please enter your annual salary in RM:"))
+# yearsEmployed = int(input("Please enter the number of years employed:"))
+
+# if (annualSalary >= 5000) and (yearsEmployed >= 2):
+#     print("You qualify for credit card application!")
+# else:
+#     print("Sorry, you do not qualify for credit card application.")
+
+# 5
+
+# print("Enter the date in the following format:[dd/mm/yy]")
+# day = int(input("What is the day? :"))
+# month = int(input("What is the month? :"))
+# year = int(input("What is the year? :"))
+
+# if month * day == year:
+#     print(day, "/", month, "/", year, "is a magic date!")
+# else:
+#     print("Unfortunately", ",", day, "/", month, "/", year, "is not a magic date.")
